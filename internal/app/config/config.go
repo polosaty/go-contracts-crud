596b8264f666e664e243c633170b4d2ecf435955
polosaty <m.aksarin@gmail.com>
2022-07-14 23:35:55 +0500

    init

---
+++ internal/app/config/config.go
@@ -0,0 +1,6 @@
+package config
+
+type Config struct {
+	RunAddress  string `env:"RUN_ADDRESS" envDefault:"localhost:8080"`
+	DatabaseURI string `env:"DATABASE_URI"`
+}
---
+++ internal/app/server/server.go
@@ -0,0 +1,18 @@
+package server
+
+import (
+	"github.com/polosaty/go-contracts-crud/internal/app/handlers"
+	"github.com/polosaty/go-contracts-crud/internal/app/storage"
+	"net/http"
+)
+
+func Serve(addr string, accrualSystemAddress string, db storage.Repository) error {
+	handler := handlers.NewMainHandler(db)
+
+	server := &http.Server{
+		Addr:    addr,
+		Handler: handler,
+	}
+
+	return server.ListenAndServe()
+}
